725bbd0d9f27089c63fd1b00b57ba267803ad489
Erdene-Ochir Mendsaikhan <ts.eric@yahoo.com>
2020-04-11 20:07:53 -0400

    update

---
+++ src/test/java/com/automation/tests/vytrack/login/loginPageTests.java
@@ -0,0 +1,60 @@
+package com.automation.tests.vytrack.login;
+
+import com.automation.utilities.BrowserUtilities;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+//Static import of all ASSERTIONS
+import static org.testng.Assert.*;
+
+public class loginPageTests {
+
+    private WebDriver driver;
+    private String URL = "https://qa2.vytrack.com/user/login";
+    private By usernameBy = By.id("prependedInput");
+    private By passwordBy = By.id("prependedInput2");
+    private By warningMessageBy = By.xpath("//*[@id='login-form']/fieldset/div[1]/div");
+
+
+
+    @Test (description = "verify that warning message displays when user enters invalid username.")
+    public void invalidUsername (){
+        driver.findElement(usernameBy).sendKeys("invalidusername");
+        driver.findElement(passwordBy).sendKeys("USerUser123", Keys.ENTER);
+        BrowserUtilities.wait(2);
+        WebElement warningElement = driver.findElement(warningMessageBy);
+        assertTrue(warningElement.isDisplayed());
+
+        String expected = "Invalid user name or password.";
+        String actual = warningElement.getText();
+        assertEquals(actual,expected);
+    }
+
+
+
+    @BeforeMethod
+    public void setup(){
+        WebDriverManager.chromedriver().version("79").setup();
+        driver = new ChromeDriver();
+        driver.get(URL);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void teardonw(){
+        // if webdriver object alive
+        if (driver != null){
+            //close browser, close session
+            driver.quit();
+            // destroy driver object for sure
+            driver = null;
+        }
+    }
+}

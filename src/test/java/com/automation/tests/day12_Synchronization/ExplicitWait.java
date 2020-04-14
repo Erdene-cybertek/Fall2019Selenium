19a218c7a8dfc898e3523914f900562b02c4b001
Erdene-Ochir Mendsaikhan <ts.eric@yahoo.com>
2020-04-14 18:55:17 -0400

    update

--- src/test/java/com/automation/tests/day12_Synchronization/ExplicitWait.java
+++ src/test/java/com/automation/tests/day12_Synchronization/ExplicitWait.java
@@ -2,14 +2,17 @@ package com.automation.tests.day12_Synchronization;
 
 import com.automation.utilities.BrowserUtilities;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class ExplicitWait {
+public class ExplicitWait {   // shuud garch irdgui buffering hiigeed loading hiigeed garch irdeg zuils
     private WebDriver driver;
 
     @BeforeMethod
@@ -26,6 +29,56 @@ public class ExplicitWait {
         // wait up to 10 seconds until title contains Google
         wait.until(ExpectedConditions.titleContains("Google"));
 
+        driver.navigate().to("https://amazon.com");
+        wait.until(ExpectedConditions.titleContains("Amazon"));
+    }
+
+    @Test
+    public void WaitForVisibility(){
+        driver.get("http://practice.cybertekschool.com/dynamic_loading/1");
+
+        WebDriverWait wait = new WebDriverWait(driver,10);
+
+        // click on start button
+        driver.findElement(By.tagName("button")).click();
+
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        wait.until(ExpectedConditions.visibilityOf(username)).sendKeys("tomsmith");
+        wait.until(ExpectedConditions.visibilityOf(password)).sendKeys("SuperSecretPassword");
+
+        wait.until(ExpectedConditions.visibilityOf(submitBtn));
+        wait.until(ExpectedConditions.elementToBeClickable(submitBtn)).click();
+
+        String expected = "Welcome to the Secure Area. When you are done click logout below.";
+        String actual = driver.findElement(By.className("subheader")).getText();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void elementToBeClickableTest(){
+        driver.get("http://practice.cybertekschool.com/dynamic_loading/5"); // When you enter this page loads little longer
+
+        WebDriverWait wait = new WebDriverWait(driver,10);
+
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("loadingoverlay")));
+        wait.until(ExpectedConditions.elementToBeClickable(submitBtn));
+
+        username.sendKeys("tomsmith");
+        password.sendKeys("SuperSecretPassword");
+        submitBtn.click();
+
+        String expected = "Welcome to the Secure Area. When you are done click logout below.";
+        String actual = driver.findElement(By.className("subheader")).getText();
+
+        Assert.assertEquals(actual, expected);
     }
 
     @AfterMethod

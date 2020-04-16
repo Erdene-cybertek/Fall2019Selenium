18bba23bf47f65b853531c6eed5358ebee7e2f4d
Erdene-Ochir Mendsaikhan <ts.eric@yahoo.com>
2020-04-16 16:07:44 -0400

    day14 page object

--- src/test/java/com/automation/pages/LoginPage.java
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -1,6 +1,8 @@
 package com.automation.pages;
 
+import com.automation.utilities.ConfigurationReader;
 import com.automation.utilities.Driver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -16,16 +18,39 @@ public class LoginPage {
     @FindBy(id = "_submit")
     public WebElement login;
 
+    @FindBy(linkText = "Forgot your password?")
+    public WebElement forgotPassword;
 
 
-    public LoginPage(){
-        // tp connect our webdriver, page class and page factory
+    public LoginPage() {
+        // tp connect our webDriver, page class and page factory
         // PageFactory - used to use @FindBy annotations
         // PageFactory - helps tp find element easier
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    /**
+     * Mthod to login, version #1
+     *Login as a Specific user
+     * @param usernameValue
+     * @param passwordValue
+     */
+    public void login(String usernameValue, String passwordValue) {
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+    }
+
+    // Overload this method // same name same credentials
+
+    /**
+     * Method to login, version #2
+     * Login as a default user
+     * Credentials will be retrieved from configuration.properties file
+     */
+    public void login() {
+        username.sendKeys(ConfigurationReader.getProperty("store_manager"));
+        password.sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
 
 
-
+    }
 }
